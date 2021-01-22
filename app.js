cb8f3b66d5aa5ec917a6057e6cb0eb62b90a199d
maya <maya.salcedo07@gmail.com>
2021-01-22 21:55:40 +0200

    add database

--- app.js
+++ app.js
@@ -1,12 +1,47 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js"); //require the local file module
-
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(express.static("public")); 
-const items = ["Go grocery", "Pick up package", "Pay bills"];
-const workItems = [];
+
+//connect to mongoose 
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+
+//make a schema
+const itemsSchema = {
+    name: String 
+};
+
+//make a model based on the schema
+const Item = mongoose.model("Item", itemsSchema);
+
+//make mongoose document
+const item1 = new Item({
+    name: "Bake cookies"
+});
+
+const item2 = new Item({
+    name: "Go grocery"
+});
+
+const item3 = new Item({
+    name: "Shovel the snow"
+});
+
+//make the array
+const defaultItems = [item1, item2, item3];
+
+const listSchema = {
+    name: String,
+    items: [itemsSchema]
+};
+
+const List = mongoose.model("List", listSchema);
+
+
+
 
 app.set('view engine', 'ejs'); //to get view engine to work with ejs, place below express(); 
 app.use(bodyParser.urlencoded({extended: true})); //this code is needed to get body-parser to work 
@@ -15,32 +50,65 @@ app.get("/", function (req, res) {
     
     const day = date.getDate(); //calls the local module
 
-        res.render("list", {
-        listTitle: day,
-        newListItems: items
-    });
-    //render a file named list.ejs from a folder named views
+    if (defaultItems.length === 0){
+        Item.insertMany(defaultItems, function(err){
+            if (err){
+                console.log(err);
+            } else {
+                console.log("Successful");
+            }
+        });
+        //insert the defaultItems to Item collections 
+        res.redirect("/"); 
+    } else {
+        Item.find({}, function(err, toDoItems){
+            res.render("list", {
+                listTitle: day,
+                newListItems:  toDoItems
+            });  
+    })
+    }
 });
+    //render a file named list.ejs from a folder named views
+    //Rendering Database Item
+
 
 app.post("/", function(req, res){
     
-    const item = req.body.newItem; //access the input in newItem
-    if (req.body.list === "Work"){ //.list refers to the button's value
-        workItems.push(item);
-        res.redirect("/work");
-    } else {
-    items.push(item);
+    const itemName = req.body.newItem; //access the input in newItem
+    
+    const item = new Item({
+        name: itemName
+    });
+
+    item.save();
+
     res.redirect("/");
-    } //to redirect to app.get res.render; to render to item back to browser
 });
 
+app.post("/delete", function(req, res){
+    const checkedItemId = req.body.checkbox;
 
-app.get("/work", function(req, res){
-    res.render("list", {
-        listTitle: "Work",
-        newListItems: workItems
+    Item.deleteOne({_id: checkedItemId}, function(err){
+        if (err){
+            console.log(err);
+        } else {
+            res.redirect("/");
+        }
+
+    })
+})
+
+app.get("/:customListName", function(req, res){
+    const customListName = req.params.customListName;
+
+    const list = new List({
+        name: customListName,
+        items: defaultItems
     });
-}); //to create a new page for work list
+
+    list.save();
+}); //Express route parameters
 
 app.get("/about", function(req, res){
     res.render("about");
